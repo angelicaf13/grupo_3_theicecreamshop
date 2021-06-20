a3f25d5673e706bef586e81684d8adcabd6ebea2
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-06-19 20:04:49 -0500

    Avance productDetail

--- app.js
+++ app.js
@@ -6,4 +6,25 @@ app.use(express.static(publicPath));
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/index.html'));
 });
+app.get('/index',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+});
+app.get('/productCar',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/productCar.html'));
+});
+app.get('/productDetail',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'));
+});
+app.get('/productList',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/productList.html'));
+});
+app.get('/profile',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/profile.html'));
+});
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+});
 app.listen(3000,()=> console.log('Servidor corriendo en el puerto 3000'));
