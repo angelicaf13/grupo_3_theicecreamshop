83d292c8ee9519b3a5083be385a539c6affd2643
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-06-28 09:04:00 -0500

    Product

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log(publicPath);
 
 app.use(express.static(publicPath));
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'));
 });
 app.get('/index',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/index.html'));
