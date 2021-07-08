7890820576ad1931e6b37a9db6107f5bb2e4926d
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-07-08 12:57:29 -0500

    Carpetas

--- app.js
+++ app.js
@@ -8,27 +8,30 @@ console.log(publicPath);
 
 app.use(express.static(publicPath));
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+    res.sendFile(path.resolve(__dirname,'./views/main/index.html'));
 });
 app.get('/index',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+    res.sendFile(path.resolve(__dirname,'./views/main/index.html'));
 });
 app.get('/login',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+    res.sendFile(path.resolve(__dirname,'./views/users/login.html'));
 });
 app.get('/productCar',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/productCar.html'));
+    res.sendFile(path.resolve(__dirname,'./views/users/productCar.html'));
 });
 app.get('/productDetail',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'));
+    res.sendFile(path.resolve(__dirname,'./views/products/productDetail.html'));
 });
 app.get('/productList',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/productList.html'));
+    res.sendFile(path.resolve(__dirname,'./views/products/productList.html'));
 });
 app.get('/profile',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/profile.html'));
+    res.sendFile(path.resolve(__dirname,'./views/users/profile.html'));
 });
 app.get('/register',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+    res.sendFile(path.resolve(__dirname,'./views/users/register.html'));
+});
+app.get('/addProduct',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/products/addProduct.html'));
 });
 app.listen(3000,()=> console.log('Servidor corriendo en el puerto 3000'));
