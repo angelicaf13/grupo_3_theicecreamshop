e51ba907b5fb054a9f992cfc82efe810ce669fb0
angelicaf13 <figueroa.angelica@uabc.edu.mx>
2021-07-11 21:22:20 -0700

    add edit button in productList and json file to load the products data with a for loop in the ejs file

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,16 @@
 const path = require('path');
+
+const fs = require('fs');
+const jsonString = fs.readFileSync(path.resolve(__dirname,'./products.json'), 'utf-8');
+const products = JSON.parse(jsonString);
+
+
 const productsControlador = {
     detail: (req,res)=>{
         res.render(path.resolve(__dirname,'../views/products/productDetail'));
     },
     list: (req,res)=>{
-        res.render(path.resolve(__dirname,'../views/products/productList'));
+        res.render(path.resolve(__dirname,'../views/products/productList'), {listaProductos: products});
     },
     add: (req,res)=>{
         res.render(path.resolve(__dirname,'../views/products/addProduct'));
@@ -13,4 +19,5 @@ const productsControlador = {
         res.render(path.resolve(__dirname,'../views/products/updateProduct'));
     }
 }
+
 module.exports = productsControlador;
