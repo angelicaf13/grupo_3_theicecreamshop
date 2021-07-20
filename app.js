5fa3090407e55a6bf8f9905f59c44f676fa96410
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-07-19 23:02:49 -0500

    Pantalla error

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(express.static(publicPath));
 app.use(mainRouter);
 app.use(productsRouter);
 app.use(usersRouter);
+
 app.use((req, res, next) => {
-    res.status(404).render('error404');
+    res.status(404).render('./errores/error404');
    })
+
 app.listen(PORT,()=> console.log('Servidor corriendo'));
