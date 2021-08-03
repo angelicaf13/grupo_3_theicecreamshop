ca6caeea45c2a8274a5257789edc7b541c372e64
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-08-03 13:11:04 -0500

    Alert

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const fs = require('fs');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
