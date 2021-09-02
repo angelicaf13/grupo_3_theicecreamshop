9ecaa7126d982b0edba422b7dd645de7ab604d74
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-09-02 17:37:26 -0500

    Mobile menu

---
+++ public/js/selectMobile.js
@@ -0,0 +1,8 @@
+function ver() {
+    var buttonlist = document.getElementById('mobile-option-list');
+    if (buttonlist.style.display == "none") {
+        buttonlist.style.display = "flex";
+    } else {
+        buttonlist.style.display = "none";
+  }
+}
