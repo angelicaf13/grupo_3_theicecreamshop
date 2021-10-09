3ef7cd1911dcec63da9f6009bb88ec26845d03c4
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-10-09 15:15:18 -0500

    Changes

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,9 +1,13 @@
 function adminMiddleware (req, res, next) {
     console.log('hola desde el admin middle');
+    if(req.session.userLogged){
     if (req.session.userLogged.id_category == 2){
         return res.redirect('/accessError');
     }
     next();
+} else {
+    return res.redirect('/accessError');
+}
 }
 
 module.exports = adminMiddleware;
