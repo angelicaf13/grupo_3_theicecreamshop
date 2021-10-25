be26179cdc008647883849a2a5823d0a2f3a53de
angelicaf13 <figueroa.angelica@uabc.edu.mx>
2021-10-24 19:49:38 -0700

    added mobile search and filter options

---
+++ public/js/showFiltersMobile.js
@@ -0,0 +1,7 @@
+function filtersMobile(option){
+    const mobileSearchFilter = document.querySelector(".search-filter-mobile");
+
+    option == 'show' ? mobileSearchFilter.style.display = "flex" : mobileSearchFilter.style.display = "none";
+    
+
+}
